8bcd1085c298813045685bf0b32ddad4be45f264
alejandrorocha94 <rochabrian99@gmail.com>
2022-07-04 20:08:10 -0300

    cuarto commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+
 function mostrar() // inicializo la funcion mostrar
 {
-	alert("Esto funciona de Maravilla"); //muestro el mensaje
+	let mensaje;
+	mensaje = "Esto funciona de Maravilla";
+	alert(mensaje); //muestro el mensaje
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,8 +3,10 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar() // inicializo la funcion mostrar
 {
-	var dato //inicializo la variable dato aparte del var puedo utilizar el let
-	dato=prompt("ingrese dato"); //ingreso el dato
-	alert(dato);//lo muestro por pantalla 
+	let mensaje //inicializo la variable 
+
+	mensaje=prompt("ingrese dato"); //ingreso el dato
+
+	alert(meensaje);//lo muestro por pantalla 
 }
 
